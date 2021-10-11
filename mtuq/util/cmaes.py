9aab4db4f70b4f6a302d5e29b7153df7687732c0
thurinj <julienthurin06@gmail.com>
2021-10-11 14:06:57 -0400

    Ongoing work on Repair method for Boundary constraint handling.

--- mtuq/util/cmaes.py
+++ mtuq/util/cmaes.py
@@ -19,7 +19,10 @@ def inverse_linear_transform(transformed, a, b):
     i = (10*(transformed-a))/(b-a)
     return i
 
-def array_in_bounds(array, a, b):
+def in_bounds(value, a=0, b=10):
+    return value >= a and value <= b
+
+def array_in_bounds(array, a=0, b=10):
     """
     Check if all elements of an array are in bounds.
     :param array: The array to check.
@@ -31,3 +34,91 @@ def array_in_bounds(array, a, b):
         if not in_bounds(array[i], a, b):
             return False
     return True
+
+class Repair:
+    def __init__(self, method, data_array,lower_bound=0, upper_bound=10):
+        self.method = method
+        self.data_array = data_array
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+
+        # Out of boundary violators mask. l_oob for lower_out-of-boundary and u_oob for upper_out-of-boundary.
+        self.l_oob = self.data_array < self.lower_bound
+        self.u_oob = self.data_array > self.upper_bound
+
+
+        if self.method == 'reinitialize':
+            # Call the reinitialize function
+            self.reinitialize()
+        elif self.method == 'projection':
+            # Call the projection function
+            self.projection()
+        elif self.method == 'reflection':
+            # Call the reflection function
+            self.reflection()
+        elif self.method == 'wrapping':
+            # Call the wrapping function
+            self.wrapping()
+        elif self.method == 'transformation':
+            # Call the transformation function
+            self.transformation()
+        elif self.method == 'projection_to_midpoint':
+            # Call the projection_to_midpoint function
+            self.projection_to_midpoint()
+        elif self.method == 'rebound':
+            # Call the rebound function
+            self.rebound()
+        else:
+            print('Repair method not recognized')
+
+    def reinitialize(self):
+        # Redraw all out of bounds values from a uniform distribution [0,10].
+        self.data_array[self.l_oob] = np.random.uniform(self.lower_bound,self.upper_bound, len(self.data_array[self.l_oob]))
+        self.data_array[self.u_oob] = np.random.uniform(self.lower_bound,self.upper_bound, len(self.data_array[self.u_oob]))
+
+    def projection(self):
+        # Project all out of bounds values to the violated bounds.
+        self.data_array[self.l_oob] = self.lower_bound
+        self.data_array[self.u_oob] = self.upper_bound
+
+    def reflection(self):
+        # The infeasible coordinate value of the solution is reflected back from the boundary by the amount of constraint violation.
+        # This method may be call several times if the points are out of bound for more than the length of the [0,10] domain.
+        self.data_array[self.l_oob] = 2*self.lower_bound - self.data_array[self.l_oob]
+        self.data_array[self.u_oob] = 2*self.upper_bound - self.data_array[self.u_oob]
+
+    def wrapping(self):
+        # The infeasible coordinate value is shifted by the feasible interval.
+        self.data_array[self.l_oob] = self.data_array[self.l_oob] + self.upper_bound - self.lower_bound
+        self.data_array[self.u_oob] = self.data_array[self.u_oob] - self.upper_bound + self.lower_bound
+
+    def transformation(self):
+        # Defines the '90%' values treshold
+        al = np.min([(self.upper_bound - self.lower_bound)/2,(1+np.linalg.norm(self.lower_bound))/20])
+        au = np.min([(self.upper_bound - self.lower_bound)/2,(1+np.linalg.norm(self.upper_bound))/20])
+        # Create the masks for the values out of self.lower_bound - al and self.upper_bound + au bounds
+        mask_1 = self.data_array > (self.upper_bound + au)
+        mask_2 = self.data_array < (self.lower_bound - al)
+        # Reflect out of bounds values according to self.lower_bound - al and self.upper_bound + au.
+        self.data_array[mask_1] = 2*self.upper_bound + au - self.data_array[mask_1]
+        self.data_array[mask_2] = 2*self.lower_bound + al - self.data_array[mask_2]
+
+        # Create masks for the nonlinear transformation.
+        mask_3 = (self.data_array >= (self.lower_bound + al)) & (self.data_array <= (self.upper_bound - au))
+        mask_4 = (self.data_array >= (self.lower_bound - al)) & (self.data_array < (self.lower_bound + al))
+        mask_5 = (self.data_array > (self.upper_bound - au)) & (self.data_array <= (self.upper_bound + au))
+
+        # Nonlinear transform defined by R. Biedrzycki 2019, https://doi.org/10.1016/j.swevo.2019.100627
+        # Note that reflected data are transformed according to the same principle which results in a periodic transformation
+        self.data_array[mask_3] = self.data_array[mask_3]
+        self.data_array[mask_4] = self.lower_bound + (self.data_array[mask_4] - (self.lower_bound-al))**2/4*al
+        self.data_array[mask_5] = self.upper_bound - (self.data_array[mask_5] - (self.upper_bound+au))**2/4*al
+
+
+    def projection_to_midpoint(self):
+        # Project the particles onto the domain using the midpoint method
+        pass
+
+    def rebound(self):
+        # Rebound the particles
+        pass
