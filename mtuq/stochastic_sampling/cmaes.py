3da2b77affe341465ebd70e1c557da440e36f85f
thurinj <julienthurin06@gmail.com>
2021-10-28 11:31:41 -0400

    Final version of serial CMA-ES.

--- mtuq/stochastic_sampling/cmaes.py
+++ mtuq/stochastic_sampling/cmaes.py
@@ -17,7 +17,8 @@ class CMA_ES(object):
         self.n = len(self._parameters)
         self.xmean = np.asarray([[val.initial for val in self._parameters]]).T
         self.sigma = 2
-        self.origin = origin
+        self.catalog_origin = origin
+        self.counteval = 0
         if not callback_function == None:
             self.callback = callback_function
         elif len(parameters_list) == 6 or len(parameters_list) == 9:
@@ -68,7 +69,24 @@ class CMA_ES(object):
                     print('repairing '+self._parameters[_i].name+' with '+self._parameters[_i].repair+' method')
                     Repair(self._parameters[_i].repair, self.mutants[_i], self.xmean[_i])
 
-    def eval_fitness(self):
+    def mean_diff(self, new, old):
+        diff = new-old
+        for _i, param in enumerate(self._parameters):
+            if param.repair == 'wrapping':
+                angular_diff = linear_transform(new[_i], 0, 360)-linear_transform(old[_i], 0, 360)
+                angular_diff = inverse_linear_transform((angular_diff+180)%360 - 180, 0, 360)
+                diff[_i] = angular_diff
+        return diff
+
+    def circular_mean(self, id):
+        param = self.mutants[id]
+        a = linear_transform(param, 0, 360)-180
+        mean = np.rad2deg(np.arctan2(np.sum(np.sin(np.deg2rad(a[range(int(self.mu))]))*self.weights.T), np.sum(np.cos(np.deg2rad(a[range(int(self.mu))]))*self.weights.T)))+180
+        mean = inverse_linear_transform(mean, 0, 360)
+        return mean
+
+    # Evaluate the misfit for each mutant of the population.
+    def eval_fitness(self, data, stations, db, process, misfit):
         # Project each parameter in their respective physical domain, according to their `scaling` property
         self.transformed_mutants = np.zeros_like(self.mutants)
         for _i, param in enumerate(self._parameters):
@@ -82,19 +100,100 @@ class CMA_ES(object):
             # Apply optional projection operator to each parameter
             if not param.projection is None:
                 self.transformed_mutants[_i] = np.asarray(list(map(param.projection, self.transformed_mutants[_i])))
-        self.sources = np.ascontiguousarray(self.callback(*self.transformed_mutants))
 
-        # Evaluate the misfit for each mutant of the population.
+        self.sources = np.ascontiguousarray(self.callback(*self.transformed_mutants[0:6]))
+        print('creating new origins list')
+        self.create_origins()
+        for X in self.origins:
+            print(X)
 
-    def create_origins(self, **kwargs):
-        catalog_origin = self.origin
+        self.misfit_val = [misfit(data, db.get_greens_tensors(stations, origin).map(process), np.array([self.sources[_i]])) for _i, origin in enumerate(self.origins)]
+        self.misfit_val = np.asarray(self.misfit_val).T[0]
 
-        depths = np.array(
-             # depth in meters
-            [25000., 30000., 35000., 40000.,
-             45000., 50000., 55000., 60000.])
+        self.counteval += self.lmbda
+        return self.misfit_val
 
-        origins = []
-        for depth in depths:
-            origins += [catalog_origin.copy()]
-            setattr(origins[-1], 'depth_in_m', depth)
+    def fitness_sort(self, misfit):
+        # Sort by fitness
+        self.mutants = self.mutants[:,np.argsort(misfit)[0]]
+        self.transformed_mutants = self.transformed_mutants[:,np.argsort(misfit)[0]]
+
+    def update_mean(self):
+        # Update the mean
+        self.xold = self.xmean
+        self.xmean = np.array([np.sum(self.mutants[:,range(int(self.mu))]*self.weights.T, axis=1)]).T
+        for _i, param in enumerate(self._parameters):
+            if param.repair == 'wrapping':
+                print('computing wrapped mean for parameter:', param.name)
+                self.xmean[_i] = self.circular_mean(_i)
+
+
+    def update_step_size(self):
+        # Step size control
+        self.ps = (1-self.cs)*self.ps + np.sqrt(self.cs*(2-self.cs)*self.mueff) * self.invsqrtC @ (self.mean_diff(self.xmean, self.xold) / self.sigma)
+
+    def update_covariance(self):
+        # Covariance matrix adaptation
+        if np.linalg.norm(self.ps)/np.sqrt(1-(1-self.cs)**(2*self.counteval/self.lmbda))/self.chin < 1.4 + 2/(self.n+1):
+            self.hsig = 1
+        if np.linalg.norm(self.ps)/np.sqrt(1-(1-self.cs)**(2*self.counteval/self.lmbda))/self.chin >= 1.4 + 2/(self.n+1):
+            self.hsig = 0
+        self.pc = (1-self.cc) * self.pc + self.hsig * np.sqrt(self.cc*(2-self.cc)*self.mueff) * self.mean_diff(self.xmean, self.xold) / self.sigma
+
+        artmp = (1/self.sigma) * self.mean_diff(self.mutants[:,0:int(self.mu)], self.xold)
+        self.C = (1-self.c1-self.cmu) * self.C + self.c1 * (self.pc@self.pc.T + (1-self.hsig) * self.cc*(2-self.cc) * self.C) + self.cmu * artmp @ np.diag(self.weights.T[0]) @ artmp.T
+        # Adapt step size
+        self.sigma = self.sigma * np.exp((self.cs/self.damps)*(np.linalg.norm(self.ps)/self.chin - 1))
+
+        if self.counteval - self.eigeneval > self.lmbda/(self.c1+self.cmu)/self.n/10:
+            self.eigeneval = self.counteval
+            self.C = np.triu(self.C) + np.triu(self.C,1).T
+            self.D,self.B = np.linalg.eig(self.C)
+            self.D = np.array([self.D]).T
+            self.D = np.sqrt(self.D)
+            self.invsqrtC = self.B @ np.diag(self.D[:,0]**-1) @ self.B.T
+
+    def create_origins(self):
+        self.catalog_origin
+
+        depth = self.transformed_mutants[6]
+        latitude = self.transformed_mutants[7]
+        longitude = self.transformed_mutants[8]
+
+        self.origins = []
+        for i in range(len(depth)):
+            self.origins += [self.catalog_origin.copy()]
+            setattr(self.origins[-1], 'depth_in_m', depth[i])
+            setattr(self.origins[-1], 'latitude', latitude[i])
+            setattr(self.origins[-1], 'longitude', longitude[i])
+
+    def return_candidate_solution(self, id=None):
+        if not id == None:
+            return_x = np.array([self.mutants[:,id]]).T
+        else:
+            return_x = self.xmean
+        self.transformed_mean = np.zeros_like(return_x)
+        for _i, param in enumerate(self._parameters):
+            print(param.scaling)
+            if param.scaling == 'linear':
+                self.transformed_mean[_i] = linear_transform(return_x[_i], param.lower_bound, param.upper_bound)
+            elif param.scaling == 'log':
+                self.transformed_mean[_i] = logarithmic_transform(return_x[_i], param.lower_bound, param.upper_bound)
+            else:
+                raise ValueError("Unrecognized scaling, must be linear or log")
+            # Apply optional projection operator to each parameter
+            if not param.projection is None:
+                self.transformed_mean[_i] = np.asarray(list(map(param.projection, self.transformed_mean[_i])))
+
+        self.catalog_origin
+        depth = self.transformed_mean[6]
+        latitude = self.transformed_mean[7]
+        longitude = self.transformed_mean[8]
+        self.final_origin = []
+        for i in range(len(depth)):
+            self.final_origin += [self.catalog_origin.copy()]
+            setattr(self.final_origin[-1], 'depth_in_m', depth[i])
+            setattr(self.final_origin[-1], 'latitude', latitude[i])
+            setattr(self.final_origin[-1], 'longitude', longitude[i])
+
+        return(self.transformed_mean, self.final_origin)
